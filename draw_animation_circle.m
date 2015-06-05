257fa541c8331437ffb02cf3c438c27acd641b5d
shaoguangleo <shaoguangleo@gmail.com>
2015-06-05 17:44:17 +0800

    Add avoid close function
    avoid close before finish plotting.

--- draw_animation_circle.m
+++ draw_animation_circle.m
@@ -5,4 +5,8 @@ r = 10;
 x = r*cos(theta);
 y = r*sin(theta);
 
+%do not allow close the figure before the plot finished
+set(gcf,'CloseRequestFcn','');
 comet(x,y);
+%resume the close function on the right-upper corner
+set(gcf,'closeRequestFcn','closereq');
